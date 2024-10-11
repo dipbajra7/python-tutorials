bc22ada7ca497f5beec35e70f003727845de8335
dipbajra7 <dipbajra7+github@gmail.com>
2024-10-11 19:47:37 -0400

    search algorithms

---
+++ programs/search-algorithm.py
@@ -0,0 +1,57 @@
+import math
+
+def search(number_search, numbers:list):
+    numbers.sort()
+    numbers2 = numbers
+    search = number_search
+    index_size = len(numbers)-1
+    count = 0
+    i = 0
+    not_found = True
+    while(not_found):
+        count+=1
+        i = math.floor(index_size/2)
+        if(search == numbers[i]):
+            print(f'search ----> found {search} at index {numbers2.index(search)} in {count} tries')
+            not_found =False
+            break
+        elif(search > numbers[i]):
+            numbers = numbers[i+1:index_size+1]
+            index_size = len(numbers)-1
+        else:
+            numbers = numbers[0:i]
+            index_size = len(numbers)-1
+      
+
+
+def sequential_search(search_number, numbers:list):
+    #sequential/liner/simple search
+    numbers.sort()
+    search = search_number
+    count = 0
+    for number in numbers:
+        count+=1
+        if(search == number):
+            print(f'sequential -----> found {search} at index {numbers.index(search)} in {count} tries')
+            break
+
+
+
+
+
+entered_number = int(input('enter number: '))
+numbers = list(range(1,101))
+search(entered_number, numbers)
+# for i in numbers:  
+#     search(i, numbers)
+
+# print('================================================')
+
+for i in range(1,101):
+    sequential_search(i, numbers)
+
+
+
+
+
+
